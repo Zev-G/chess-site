75d278ce414ae23f7a18d71f812fdd04ea6736d8
TheMisterWolf <vezvez1234@gmail.com>
2022-04-08 17:01:48 -0700

    Changed title to Chess and icon to white king

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import GameSettings from "../components/GameSettings.svelte";
     import ChessBoard from "../components/ChessBoard.svelte";
     import Game from "../components/Game";
 </script>
+
+<svelte:head>
+    <title>Chess</title>
+</svelte:head>
 
 <ChessBoard game={new Game()}/>
 
